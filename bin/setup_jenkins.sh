841dba6e7202068d3bc26672458101e5c67ab2bb
Joseph McDade <JMcDade42@gmail.com>
2019-06-18 16:07:58 -0500

    Configure setup_jenkins.sh

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -14,12 +14,19 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 
 # Set up Jenkins with sufficient resources
 # TBD
+oc project ${GUID}-jenkins
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi \
+	--param DISABLE_ADMINISTRATIVE_MONITORS=true --name=jenkins
 
 # Create custom agent container image with skopeo
 # TBD
+oc new-build  -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\n
+	USER root\n RUN yum -y install skopeo && yum clean all\n
+	USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
 # TBD
+cat pipeline.yaml | oc create -f - -n ${GUID}-jenkins
 
 # Make sure that Jenkins is fully up and running before proceeding!
 while : ; do
